4124b64e753a79b80f5926d7840f826fe4d30c2d
Gevik Babakhani <gevik@blendjs.com>
2019-07-18 09:25:38 +0200

    added isNullOrUndefDefault

--- src/isNullOrUndef.ts
+++ src/isNullOrUndef.ts
@@ -8,3 +8,17 @@
 export function isNullOrUndef(value: any): boolean {
     return value === null || value === undefined || value === "undefined";
 }
+
+/**
+ * Tests whether the given value is null or undefined and returns
+ * a default value in case.
+ *
+ * @export
+ * @template ReturnType
+ * @param {ReturnType} value
+ * @param {ReturnType} defaultValue
+ * @returns {ReturnType}
+ */
+export function isNullOrUndefDefault<ReturnType>(value: ReturnType, defaultValue: ReturnType): ReturnType {
+    return isNullOrUndef(value) ? defaultValue : value;
+}
